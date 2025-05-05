dbf8c2a84868d038c9b02ac2aeff9e276a796d93
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-05-05 19:59:59 +0545

    second

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,21 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +29,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",         
+      "react/no-unescaped-entities": "off",         
+      "@typescript-eslint/no-unused-vars": "warn",  
+    },
+  },
 ];
 
 export default eslintConfig;
