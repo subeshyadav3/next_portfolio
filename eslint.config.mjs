3d7753e9a68598a23e0d96c736f4409dc1ca2cdc
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-05-05 20:27:31 +0545

    second

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@next/next/no-img-element": "warn", // or "off"
+      "react/no-unescaped-entities": "warn", // or "off"
+      "@typescript-eslint/no-unused-vars": "warn", // or "off"
+    },
+  },
 ];
+
 
 export default eslintConfig;
