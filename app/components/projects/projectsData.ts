9148ceb4d352a8fbd3dcf1edb1a9374c538949fc
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2026-01-06 19:13:40 +0545

    Updated Resume With Egov Project

--- app/components/projects/projectsData.ts
+++ app/components/projects/projectsData.ts
@@ -1,61 +1,75 @@
 const projectsData = [
-    {
-      title: "Tech Store ~ MERN Stack",
-      description: "A full-stack e-commerce website developed using the MERN stack, admin and user dashboard and all features.",
-      photo: ["/projects/store.png","/projects/store1.png", "/projects/store2.png"],
-      stack: ["React", "NodeJS", "ExpressJS", "MongoDB"],
-      projectLink: "https://frontendstore-five.vercel.app/",
-      githubLink: "https://github.com/subeshyadav3/store",
-   
-    },
-    {
-      title: "Next.js Full Stack Blog with AI Summarization",
-      description: " Leveraged Next.js with API routes, Prisma ORM, and PostgreSQL for backend; used Shadcn UI for responsive and polished frontend design.",
-      photo: ["/projects/blog.png", "/projects/blog1.png", "/projects/blog2.png"],
-      stack: ["Next.js", "TypeScript", "Tailwind", "Clerk","ShadCn","OpenAI"],
-      projectLink: "https://blog-app-tau-ashy.vercel.app/",
-      githubLink: "https://github.com/subeshyadav3/blog_app",
-    
-    },
-    {
-      title: "NPL API",
-      description: "This is a comprehensive API for the Nepal Premier League, providing all the necessary data to facilitate in-depth analysis and insights.",
-      photo: "/projects/npl.png",
-      stack: ["NodeJS", "ExpressJS"],
-      projectLink: "https://nepal-premiere-league-npl-api.vercel.app/",
-      githubLink: "https://github.com/subeshyadav3/Nepal_Premier_League-NPL-_API",
-    
-    },
-    {
-      title: "AQI Predictor",
-      description: "An end-to-end data science project that analyzes historical air quality trends in Kathmandu, predicts AQI using machine learning, and provides real-time forecasting using Facebook Prophet — all wrapped in an interactive Streamlit dashboard.",
-      photo: [
-        "/projects/aqi.png",
-        "/projects/aqi1.png"
-      ],
-      stack: ["Python", "Streamlit", "Pandas/Numpy"],
-      projectLink: "https://kathmanduairqualityforecasting.streamlit.app/",
-      githubLink: "https://github.com/subeshyadav3/AQI_Prediction_Kathmandu",
-   
-    },
-    {
-      title: "Designed Portfolio Website",
-      description: "A sleek, responsive portfolio website built using Next.js, styled with Tailwind CSS, and enhanced with shadcn/ui components. It showcases projects, skills, and contact options in an elegant single-page layout with smooth transitions and modern design aesthetics.",
-      photo: "/projects/portfolio.png",
-      stack: ["Next.js", "Tailwind CSS", "shadcn/ui"],
-      projectLink: "https://paudelsangam.com.np/",
-      githubLink: "https://github.com/subeshyadav3/sangam_portfolio_design",
-    },
-    {
-      title: "Flappy Bird",
-      description: "A custom implementation of the classic Flappy Bird game, developed with my own logic to deepen my understanding and enhance my skills in JavaScript.",
-      photo: "/projects/flappy.png",
-      stack: ["HTML", "CSS", "JavaScript"],
-      projectLink: "https://subeshyadav3.github.io/flappybird/",
-      githubLink: "https://github.com/subeshyadav3/flappybird",
-    
-    }
-  ]
-  
+  {
+    title: "Transparent Nepal: Full-Stack E-Governance Portal",
+    description: "Architectured a secure e-governance platform for real-time budget tracking and infrastructure monitoring; implemented role-based access control and immutable ledger visualization.",
+    photo: [
+      "/projects/egov/home.png",
+      "/projects/egov/citizen.png",
+      "/projects/egov/project_main.png",
+      "/projects/egov/project_inside.png",
+      "/projects/egov/analytics.png"
+    ],
+    stack: ["Next.js", "TypeScript", "Tailwind", "NextAuth", "ShadCn", "Prisma", "PostgreSQL"],
+    projectLink: "https://egov-nepal.vercel.app/",
+    githubLink: "https://github.com/subeshyadav3/Transparent_Nepal_eGovernance_Project",
+  },
+  {
+    title: "Tech Store ~ MERN Stack",
+    description: "A full-stack e-commerce website developed using the MERN stack, admin and user dashboard and all features.",
+    photo: ["/projects/store.png", "/projects/store1.png", "/projects/store2.png"],
+    stack: ["React", "NodeJS", "ExpressJS", "MongoDB"],
+    projectLink: "https://frontendstore-five.vercel.app/",
+    githubLink: "https://github.com/subeshyadav3/store",
+
+  },
+  {
+    title: "Next.js Full Stack Blog with AI Summarization",
+    description: " Leveraged Next.js with API routes, Prisma ORM, and PostgreSQL for backend; used Shadcn UI for responsive and polished frontend design.",
+    photo: ["/projects/blog.png", "/projects/blog1.png", "/projects/blog2.png"],
+    stack: ["Next.js", "TypeScript", "Tailwind", "Clerk", "ShadCn", "OpenAI"],
+    projectLink: "https://blog-app-tau-ashy.vercel.app/",
+    githubLink: "https://github.com/subeshyadav3/blog_app",
+
+  },
+  {
+    title: "NPL API",
+    description: "This is a comprehensive API for the Nepal Premier League, providing all the necessary data to facilitate in-depth analysis and insights.",
+    photo: "/projects/npl.png",
+    stack: ["NodeJS", "ExpressJS"],
+    projectLink: "https://nepal-premiere-league-npl-api.vercel.app/",
+    githubLink: "https://github.com/subeshyadav3/Nepal_Premier_League-NPL-_API",
+
+  },
+  {
+    title: "AQI Predictor",
+    description: "An end-to-end data science project that analyzes historical air quality trends in Kathmandu, predicts AQI using machine learning, and provides real-time forecasting using Facebook Prophet — all wrapped in an interactive Streamlit dashboard.",
+    photo: [
+      "/projects/aqi.png",
+      "/projects/aqi1.png"
+    ],
+    stack: ["Python", "Streamlit", "Pandas/Numpy"],
+    projectLink: "https://kathmanduairqualityforecasting.streamlit.app/",
+    githubLink: "https://github.com/subeshyadav3/AQI_Prediction_Kathmandu",
+
+  },
+  {
+    title: "Designed Portfolio Website",
+    description: "A sleek, responsive portfolio website built using Next.js, styled with Tailwind CSS, and enhanced with shadcn/ui components. It showcases projects, skills, and contact options in an elegant single-page layout with smooth transitions and modern design aesthetics.",
+    photo: "/projects/portfolio.png",
+    stack: ["Next.js", "Tailwind CSS", "shadcn/ui"],
+    projectLink: "https://paudelsangam.com.np/",
+    githubLink: "https://github.com/subeshyadav3/sangam_portfolio_design",
+  },
+  {
+    title: "Flappy Bird",
+    description: "A custom implementation of the classic Flappy Bird game, developed with my own logic to deepen my understanding and enhance my skills in JavaScript.",
+    photo: "/projects/flappy.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    projectLink: "https://subeshyadav3.github.io/flappybird/",
+    githubLink: "https://github.com/subeshyadav3/flappybird",
+
+  }
+]
+
 
 export default projectsData;
