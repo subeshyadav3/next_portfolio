37a18ec433ade7bd721d294940e381dd66af8f78
Subesh <subesh@local>
2026-06-30 09:59:51 +0000

    fix: remove remaining Newsletter usage from search page

--- app/(blog)/blog/search/page.tsx
+++ app/(blog)/blog/search/page.tsx
@@ -42,9 +42,6 @@ export default function SearchPage() {
         />
       </section>
 
-      <section className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
-        <Newsletter />
-      </section>
     </>
   );
 }
